2124110a8e948a2354539945379b05771cd74c55
migellebarlis <migellejosebarlis@gmail.com>
2022-10-20 11:40:26 +0800

    added tests for invalid email

--- ServicesTests/EmailServiceTest.cs
+++ ServicesTests/EmailServiceTest.cs
@@ -52,6 +52,24 @@ public class EmailServiceTest
         var textBox = driver.FindElement(By.CssSelector("#identifierId"));
         var submitButton = driver.FindElement(By.CssSelector("#identifierNext > div > button"));
 
+        textBox.SendKeys("test @gmail.com");
+        submitButton.Click();
+
+        var message = driver.FindElement(By.CssSelector("#yDmH0d > c-wiz > div > div.eKnrVb > div > div.j663ec > div > form > span > section > div > div > div.d2CFce.cDSmF.cxMOTc > div > div.LXRPh > div.dEOOab.RxsGPe > div"));
+        var value = message.Text;
+        Assert.AreEqual("Enter a valid email or phone number", value);
+    }
+
+    [Test]
+    public void NavigateToWebsite_RejectsNonExistingEmail()
+    {
+        driver.Navigate().GoToUrl("https://mail.google.com/");
+
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(1000);
+
+        var textBox = driver.FindElement(By.CssSelector("#identifierId"));
+        var submitButton = driver.FindElement(By.CssSelector("#identifierNext > div > button"));
+
         textBox.SendKeys("test@gmail.com");
         submitButton.Click();
 
