6e2a2929fc629476ad5910f6b263859215b0fc8a
migellebarlis <migellejosebarlis@gmail.com>
2022-10-20 11:30:14 +0800

    updated tests for gmail

--- ServicesTests/EmailServiceTest.cs
+++ ServicesTests/EmailServiceTest.cs
@@ -20,44 +20,43 @@ public class EmailServiceTest
     }
 
     [Test]
-    public void EmailInput_RejectsInvalidEmail()
+    public void NavigateToWebsite_ReturnsWebsiteName()
     {
-        driver.Navigate().GoToUrl("https://www.selenium.dev/selenium/web/web-form.html");
+        driver.Navigate().GoToUrl("https://mail.google.com/");
+
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(1000);
 
         var title = driver.Title;
-        Assert.AreEqual("Web form", title);
-
-        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(500);
-
-        var textBox = driver.FindElement(By.CssSelector("#my-text-id"));
-        var submitButton = driver.FindElement(By.TagName("button"));
-
-        textBox.SendKeys("Selenium");
-        submitButton.Click();
-
-        var message = driver.FindElement(By.Id("message"));
-        var value = message.Text;
-        Assert.AreEqual("Received!", value);
+        Assert.AreEqual("Gmail", title);
     }
 
     [Test]
-    public void EmailInput_AcceptsValidEmail()
+    public void NavigateToWebsite_ReturnsEmailInput()
     {
-        driver.Navigate().GoToUrl("https://www.selenium.dev/selenium/web/web-form.html");
+        driver.Navigate().GoToUrl("https://mail.google.com/");
 
-        var title = driver.Title;
-        Assert.AreEqual("Web form", title);
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(1000);
 
-        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(500);
+        var textBox = driver.FindElement(By.CssSelector("#identifierId"));
 
-        var textBox = driver.FindElement(By.CssSelector("#my-text-id"));
-        var submitButton = driver.FindElement(By.TagName("button"));
+        Assert.IsNotNull(textBox);
+    }
 
-        textBox.SendKeys("Selenium");
+    [Test]
+    public void NavigateToWebsite_RejectsInvalidEmail()
+    {
+        driver.Navigate().GoToUrl("https://mail.google.com/");
+
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(1000);
+
+        var textBox = driver.FindElement(By.CssSelector("#identifierId"));
+        var submitButton = driver.FindElement(By.CssSelector("#identifierNext > div > button"));
+
+        textBox.SendKeys("test@gmail.com");
         submitButton.Click();
 
-        var message = driver.FindElement(By.Id("message"));
+        var message = driver.FindElement(By.CssSelector("#yDmH0d > c-wiz > div > div.eKnrVb > div > div.j663ec > div > form > span > section > div > div > div.d2CFce.cDSmF.cxMOTc > div > div.LXRPh > div.dEOOab.RxsGPe > div"));
         var value = message.Text;
-        Assert.AreEqual("Received!", value);
+        Assert.AreEqual("Couldn’t find your Google Account", value);
     }
 }
